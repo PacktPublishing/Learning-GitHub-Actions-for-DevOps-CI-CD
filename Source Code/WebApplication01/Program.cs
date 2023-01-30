d62b5b19f758ea2349b7c6004032c07ed2bb5d4b
sanjeetkumar13 <41476387+sanjeetkumar13@users.noreply.github.com>
2023-01-30 13:39:31 +0530

    Delete

--- Source Code/WebApplication01/Program.cs
+++
@@ -1,6 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello GitHub Action!!!  TechSckool is now live with latest code");
-
-app.Run();
